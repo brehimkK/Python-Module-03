b546d29e45865d13645e16527d7ef4c1b4afa5ea
Brahim El bourkali <brel-bou@c1r7p5.1337.ma>
2026-03-05 21:57:02 +0000

    .

---
+++ ex6/ft_analytics_dashboard.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+
+def main ()-> None:
+	print("=== Game Analytics Dashboard ===")
+	
+	players = ["alice", "bob", "charlie"]
+	scores = {
+		"alice": 2300,
+		"bob": 1800,
+		"charlie": 2150,
+		"diana": 2050
+		}
+	
